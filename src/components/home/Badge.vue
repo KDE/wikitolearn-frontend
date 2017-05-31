37112eb88f0c8c08f5b3d6f3a7ac449b5a319dd4
Cristian Baldi <bld.cris.96@gmail.com>
2017-05-31 18:38:30 +0200

    [components/home/badge] Create first implementation of the home screen badge
    This is just a proof of concept

---
+++ src/components/home/Badge.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+	.Badge
+		// TODO: replace with router-link
+		a.Badge__name(href="#") {{ name }}
+</template>
+
+<style lang="scss">
+.Badge {
+	padding: 1rem;
+	margin-bottom: 1rem;
+	border: 1px solid black;
+}
+</style>
+
+<script>
+export default {
+	name: "Badge",
+	props: {
+		name: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
