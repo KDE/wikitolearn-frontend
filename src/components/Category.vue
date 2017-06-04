848887cdd7ccebe37b625c9cf3d1ad383b12bd04
Cristian Baldi <bld.cris.96@gmail.com>
2017-06-04 12:37:32 +0200

    [components/Category] Add placeholder Category component

---
+++ src/components/Category.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+	.Category
+		h3.Category__name(v-if="showName") {{ categoryName }}
+		.Category__subcategories
+			b Categories
+			.Category__subcategory Programming
+			.Category__subcategory Database
+			.Category__subcategory Maths
+		.Category__courses
+			b Uncategorized Courses
+			.Category__course Developing Android Apps
+			.Category__course Computability
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+export default {
+	name: "Category",
+	props: {
+		categoryName: {
+			type: String,
+			required: true
+		},
+		showName: {
+			type: Boolean,
+			default: true
+		}
+	}
+}
+</script>
