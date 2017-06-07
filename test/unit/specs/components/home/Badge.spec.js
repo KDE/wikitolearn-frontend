a541d4d8e3b41823dbdbc4d7edd851adb2bffb96
Cristian Baldi <bld.cris.96@gmail.com>
2017-06-07 15:19:11 +0200

    [test] Add test for home/Badge component

---
+++ test/unit/specs/components/home/Badge.spec.js
@@ -0,0 +1,17 @@
+import { mount } from "avoriaz"
+import { expect } from "chai"
+import Badge from "components/home/Badge"
+
+describe("Badge", () => {
+	it("renders a name", () => {
+		const badge = mount(Badge, {
+			propsData: {
+				departmentName: "Test"
+			}
+		})
+
+		expect(badge.is("div")).to.equal(true)
+		expect(badge.hasClass("Badge")).to.equal(true)
+		expect(badge.find(".Badge__name")[0].text()).to.equal("Test")
+	})
+})
