d2f3f6dfd4b9129386dad32518e72a0d0915158d
Cristian Baldi <bld.cris.96@gmail.com>
2017-06-19 16:28:21 +0200

    [store] Add actions to fetch categories

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,2 +1,16 @@
+import { Categories } from "api/Categories"
+
 export const actions = {
+	FETCH_ROOT_CATEGORIES({ commit }) {
+		return Categories.getRootCategories()
+			.then((response) => {
+				commit("SET_ROOT_CATEGORIES", { categories: response.data })
+			})
+	},
+	FETCH_CATEGORY({ commit }, { categoryName }) {
+		return Categories.get(categoryName)
+			.then((response) => {
+				commit("SET_CATEGORY", { category: response.data })
+			})
+	}
 }
