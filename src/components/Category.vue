c76543685b311555a29ec799f47f30e722ee608a
Cristian Baldi <bld.cris.96@gmail.com>
2017-06-20 10:30:48 +0200

    [components/Category] Improve visualization of Categories

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,17 +1,30 @@
 <template lang="pug">
 	.Category
 		h3.Category__name(v-if="showName") {{ category.display_name }}
-		.Category__subcategories
-			b Categories
-			.Category__subcategory(v-for="subcategory in category.subcategories") {{ subcategory.display_name }}
+		.Category__subcategories(v-if="category.subcategories")
+			b Subcategories
+			Category(
+				v-for="subcategory in category.subcategories"
+				:category="subcategory"
+				:key="subcategory.name"
+			)
 		.Category__courses
-			b Uncategorized Courses
-			.Category__course Developing Android Apps
-			.Category__course Computability
+			b Courses
+			ul
+				li(v-for="course in category.courses")
+					a.Category__course(href="#") {{ course.display_name }}
 </template>
 
 <style lang="scss">
+.Category {
+	padding: 1rem;
+	margin-bottom: 2rem;
+	border: 1px solid black;
 
+	&__subcategories {
+		margin-bottom: 1rem;
+	}
+}
 </style>
 
 <script>
