5fefada1992ac5069dec5c8dcbf83ee6f1f7c497
Cristian Baldi <bld.cris.96@gmail.com>
2017-07-03 18:11:05 +0200

    [components/Editor] Start working on a component integrating CkEditor

---
+++ src/components/Editor.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+	.Editor
+		NoSSR
+			textarea(id="foo", :value="content")
+</template>
+
+
+<script>
+import Vue from "vue"
+import NoSSR from "vue-no-ssr"
+
+export default {
+	name: "Editor",
+	components: { NoSSR },
+	props: {
+		content: {
+			type: String
+		}
+	},
+	mounted() {
+		Vue.nextTick(() => {
+			window.CKEDITOR.replace("foo", {
+				removeButtons: "About",
+				customConfig: "",
+				height: "100vh"
+			})
+		})
+	}
+}
+</script>
