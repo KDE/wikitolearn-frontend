3012cf2b20bde4277003cdfc13861f4833b12ae5
Cristian Baldi <bld.cris.96@gmail.com>
2017-08-12 15:48:32 +0200

    [components/SessionInfo] Better integration with keycloak

--- src/components/SessionInfo.vue
+++ src/components/SessionInfo.vue
@@ -2,21 +2,20 @@
 	NoSSR
 		.SessionInfo
 			template(v-if="$keycloak")
-				a(v-if="authenticated()") You are logged in
-				a(v-else) Not logged in
 				WTLButton(@click=`doLogin()`) Login
 				WTLButton(@click=`$keycloak.logout()`) Logout
-				WTLButton(@click=`authenticated()`) Check
-				p {{ $keycloak.authenticated }}
+				WTLButton(@click=`checkLogin()`) Get User Data
 </template>
 
 <script>
 export default {
 	name: "SessionInfo",
 	methods: {
-		authenticated() {
-			console.log(this.$keycloak.authenticated)
-			return this.$keycloak.authenticated
+		checkLogin() {
+			const _this = this
+			this.$keycloak.loadUserProfile().success((user) => {
+				_this.$dialog.alert({ content: JSON.stringify(user) })
+			})
 		},
 		doLogin() {
 			this.$keycloak.login({
